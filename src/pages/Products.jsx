cf52c680e23a7ded7f3e0e2681b25f71b1c3e04a
Legend cruiz <legendcruiz@gmail.com>
2026-05-26 21:40:08 +0100

    fixed ProductDetails import

--- src/pages/Products.jsx
+++
@@ -1,9 +0,0 @@
-function Products() {
-  return (
-    <div className='max-w-7xl mx-auto p-6'>
-      <h1 className='text-4xl font-bold'>All Products</h1>
-    </div>
-  )
-}
-
-export default Products
