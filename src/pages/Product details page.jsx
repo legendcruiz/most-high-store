420ecbaaaaacef5aeeeadcb4c9ca3829c74cd79d
Legend cruiz <legendcruiz@gmail.com>
2026-05-27 12:00:28 +0100

    fix deployment

---
+++ src/pages/Product details page.jsx
@@ -0,0 +1,9 @@
+function Products() {
+  return (
+    <div className='max-w-7xl mx-auto p-6'>
+      <h1 className='text-4xl font-bold'>All Products</h1>
+    </div>
+  )
+}
+
+export default Products
